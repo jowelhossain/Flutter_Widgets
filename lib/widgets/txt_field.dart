01429825412346281a8fb18e3da543d05eb8f5e4
Jowel Hossain <jowelhossain117@gmail.com>
2023-05-04 17:18:11 +0600

    update

--- lib/widgets/txt_field.dart
+++ lib/widgets/txt_field.dart
@@ -8,6 +8,10 @@ class TxtField extends StatefulWidget {
 }
 
 class _TxtFieldState extends State<TxtField> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool isObscure = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +23,81 @@ class _TxtFieldState extends State<TxtField> {
         children: [
           Expanded(
             child: Container(
-              color: Color.fromARGB(255, 138, 128, 128),
+              color: Color.fromARGB(255, 82, 128, 203),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Container(
+                      child: Text(
+                        "Welcome!",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      controller: emailController,
+                      textInputAction: TextInputAction.next,
+                      decoration: InputDecoration(
+                          fillColor: Colors.white.withOpacity(.2),
+                          filled: true,
+                          //prefixIcon: Icon(Icons.email),
+                          hintText: "Enter your email",
+                          hintStyle:
+                              TextStyle(color: Colors.white.withOpacity(.6)),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Colors.white)),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Colors.white))),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextField(
+                      controller: passwordController,
+                      textInputAction: TextInputAction.done,
+                      obscureText: isObscure,
+                      decoration: InputDecoration(
+                          fillColor: Colors.white.withOpacity(.2),
+                          filled: true,
+                          //prefixIcon: Icon(Icons.email),
+                          suffixIcon: InkWell(
+                              onTap: () => setState(() {
+                                    isObscure = !isObscure;
+                                  }),
+                              child: Icon(
+                                Icons.visibility,
+                                color: Colors.white.withOpacity(.5),
+                              )),
+                          hintText: "Enter your password",
+                          hintStyle:
+                              TextStyle(color: Colors.white.withOpacity(.6)),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Colors.white)),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Colors.white))),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    MaterialButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Sign in",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: Colors.purple,
+                      hoverColor: Colors.orange,
+                    )
+                  ],
+                ),
+              ),
             ),
           )
         ],
